91b9ac66973a715a7f21a8453f4ab8a8912bd8b9
emir-kandemir <227136149+emir-kandemir@users.noreply.github.com>
2026-05-11 18:58:52 +0300

    Add sumAll function for solution
    Issue: Not works with larger number first

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,15 @@
-const sumAll = function() {
-
+const sumAll = function (stRange, endRange) {
+  let numArr = [];
+  for (let i = stRange; i <= endRange; i++) {
+    numArr.push(i);
+  }
+  console.log(numArr.sort());
+  const reduceAll = numArr.reduce((total, currentItem) => {
+    return total + currentItem;
+  });
+  return reduceAll;
 };
-
+// debugger;
+console.log(sumAll(5, 3));
 // Do not edit below this line
 module.exports = sumAll;
